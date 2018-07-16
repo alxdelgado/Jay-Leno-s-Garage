086b2e2556786a7978f0301c790b8c7f71ffc6e4
Alex Delgado <delgadoalex@icloud.com>
2018-07-16 13:40:47 -0500

    setup server and database files

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express'); 
+const app = express(); 
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override'); 
+const port = 3443; 
+
+// Require the db. 
+require('./db/db');
+
+// require middleware. 
+app.use(bodyParser.urlencoded({extended: false})); 
+app.use(methodOverride('_method')); 
+
+app.listen(port, () => {
+  console.log('Server is running on port 3443')
+}); 
+
+// main index page 
+app.get('/', (req, res) => {
+  res.render('index.ejs'); 
+}); 
+
